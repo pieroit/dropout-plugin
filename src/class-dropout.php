420b3e2548ff076336005f422ae9a125911bdeb8
pieroit <piero.savastano@gmail.com>
2016-03-23 12:08:26 +0100

    first

---
+++ src/class-dropout.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Main class exposing Dropout functionalities.
+ * Methods can be called from outside using the notation Dropout::method( params )
+ */
+class Dropout {
+	
+	public static $stopwords = array(
+		'about', 'after', 'all', 'also', 'am', 'an', 'and', 'another', 'any', 'are', 'as', 'at', 'be',
+        'because', 'been', 'before', 'being', 'between', 'both', 'but', 'by', 'came', 'can',
+        'come', 'could', 'did', 'do', 'each', 'for', 'from', 'get', 'got', 'has', 'had',
+        'he', 'have', 'her', 'here', 'him', 'himself', 'his', 'how', 'if', 'in', 'into',
+        'is', 'it', 'like', 'make', 'many', 'me', 'might', 'more', 'most', 'much', 'must',
+        'my', 'never', 'now', 'of', 'on', 'only', 'or', 'other', 'our', 'out', 'over',
+        'said', 'same', 'see', 'should', 'since', 'some', 'still', 'such', 'take', 'than',
+        'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'those',
+        'through', 'to', 'too', 'under', 'up', 'very', 'was', 'way', 'we', 'well', 'were',
+        'what', 'where', 'which', 'while', 'who', 'with', 'would', 'you', 'your',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '$', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9', '0', '_'
+	);
+	
+	/**
+	 * Measure text similarity between two posts using Jaccard/Tanimoto/Dice coefficient.
+	 * 
+	 * @param mixed $post_id_a post ID
+	 * @param mixed $post_id_b post ID
+	 * @return double A number between 1 (identity) and 0 (total diversity).
+	 */
+	public static function similarity( $post_id_a, $post_id_b ) {
+		
+		// Transform each post in a bag of words
+		$bow_a = self::post_2_bag_of_words( $post_id_a );
+		$bow_b = self::post_2_bag_of_words( $post_id_b );
+		
+		if( empty( $bow_a ) || empty( $bow_b ) ){
+			return 0;
+		}
+		
+		// Compute Tanimoto
+		$intersection = count( array_unique( array_intersect( $bow_a, $bow_b ) ) );
+		$union        = count( array_unique( array_merge( $bow_a, $bow_b ) ) );
+		
+		return $intersection / $union;
+	}
+	
+	/**
+	 * Transform post content in a bag of words
+	 * 
+	 * @param $post_id post ID
+	 * @return array Bag of words
+	 */
+	public static function post_2_bag_of_words( $post_id ) {
+		
+		$post = get_post( $post_id );
+		
+		if( is_null($post) ){
+			return array();
+		}
+		
+		$post_content  = $post->post_content;
+		$clean_content = strtolower( wp_strip_all_tags( $post_content ) );
+		
+		$clean_content = str_replace( array(".", ",", ":", ";", "!", "?", "'", '"', "(", ")" ), "", $clean_content );
+		
+		$tokens = array_unique( explode( " ", $clean_content ) );   // No punctuation
+		
+		return array_diff( $tokens, self::$stopwords );             // No common words
+	}
+}
+
---
+++ src/dropout.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Plugin Name:       Dropout
+ * Plugin URI:        http://pieroit.org/portfolio
+ * Description:       Simple recommendation engine
+ * Version:           1.0.0
+ * Author:            pieroit
+ * Author URI:        http://pieroit.org/portfolio
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       dropout
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The core plugin class
+ */
+require plugin_dir_path( __FILE__ ) . '/class-dropout.php';
+
+/**
+ * Build recommendations at post save
+ */
+function dropout_update_recommendations( $post_id ) {
+	
+	if( !in_array( get_post_type( $post_id ), array( 'post', 'page') ) ){
+		return;
+	}
+	
+	// Take latest 50 posts
+	$latest_posts = wp_get_recent_posts(
+		array(
+			'numberposts' => 10,
+			'post_type'   => array( 'post', 'page' )
+		)
+	);
+	$similarities = array();
+	
+	foreach( $latest_posts as $another_post ){
+		
+		if( $another_post['ID'] !== $post_id ) {
+			
+			// Measure similarity
+			$similarity = Dropout::similarity( $post_id, $another_post['ID'] );
+			$similarities[ $another_post['ID'] ] = $similarity;
+
+			// Update similarities for the other post
+			$already_computed_similarities = get_post_meta( $another_post['ID'], 'dropout_recommendations', true );
+			$already_computed_similarities[$post_id] = $similarity;
+			update_post_meta( $another_post['ID'], 'dropout_recommendations', $already_computed_similarities );
+		}
+	}
+	
+	// Save recommendations for this post
+	update_post_meta( $post_id, 'dropout_recommendations', $similarities );
+}
+add_action( 'save_post', 'dropout_update_recommendations' );
+
+/**
+ * Add recommendations to every post
+ */
+function dropout_get_recommendations( $content ) {
+	
+	$recommendations = get_post_meta( get_the_ID(), 'dropout_recommendations', true );
+	if( empty( $recommendations ) ) {
+		return $content;
+	}
+	
+	arsort( $recommendations );                                 // Sort by similarity
+
+	$html = '<h3>See also</h3><ul>';
+	foreach( $recommendations as $recom_id => $similarity ) {
+		$recom_post = get_post( $recom_id );
+		$html .= '<li><a href="' . get_permalink($recom_post->ID) . '">' . $recom_post->post_title . '</a> (' . $similarity .')</li>';
+	}
+	$html .= '</ul>';
+	
+	return $content . $html;
+}
+add_filter( 'the_content', 'dropout_get_recommendations' );
